f72a91023482ae4a14dfa040ecd9649531a99c5a
Smithry9 <smithry9@oregonstate.edu>
2021-04-15 16:59:39 -0700

    Get total of list, added prints

--- calc.py
+++ calc.py
@@ -4,6 +4,11 @@ def calc(x, y):
     product = x * y
     quotient = x / y
     answerList = [total, diff, product, quotient]
+    print("Answers (sum, difference, product, quotient): ", end="")
     print(answerList)
+    listTotal = 0
+    for i in answerList:
+        listTotal += i
+    print("Sum of List: " + str(listTotal))
 
 calc(10, 5)
