09fc00c6f3ce23fdfe025246e9ed2feb067820fb
Smithry9 <smithry9@oregonstate.edu>
2021-04-15 17:28:33 -0700

    added number to ascii char code

--- password.py
+++ password.py
@@ -6,6 +6,8 @@ def generate(x):
         charType = randInt(1,3)
         if(charType == 1):
             #Add a number
+            char = randInt(48,57)
+            char = chr(char)
         elif(charType == 2):
             #Add a Uppercase Letter
         else:
