d5b61f8158bef9a92f313eaee60b244d03b2f64d
Smithry9 <smithry9@oregonstate.edu>
2021-04-15 17:31:14 -0700

    adds char to password and fixed type in randint

--- password.py
+++ password.py
@@ -3,19 +3,22 @@ import random
 def generate(x):
     password = ""
     for i in range(x):
-        charType = randInt(1,3)
+        charType = randint(1,3)
         if(charType == 1):
             #Add a number
-            char = randInt(48,57)
+            char = randint(48,57)
             char = chr(char)
         elif(charType == 2):
             #Add a Uppercase Letter
-            char = randInt(65,90)
+            char = randint(65,90)
             char = chr(char)
         else:
             #Add a Lowercase Letter
-            char = randInt(97,122)
+            char = randint(97,122)
             char = chr(char)
+
+        password += char
+    print(password)
 
 print("Enter a length for the password: ", end="")
 num = int(input())
