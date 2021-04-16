4183f6b29d2668a8b29b98257201d2afcbfc5074
Smithry9 <smithry9@oregonstate.edu>
2021-04-15 17:24:35 -0700

    added a random chance for 3 different types fo characters

--- password.py
+++ password.py
@@ -2,7 +2,13 @@ import random
 
 def generate(x):
     for i in range(x):
+        charType = randInt(1,3)
+        if(charType == 1):
 
+        elif(charType == 2):
+
+        else:
+            
 
 print("Enter a length for the password: ", end="")
 num = int(input())
