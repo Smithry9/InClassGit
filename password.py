f66fda9146c8e33493eb02d39d72c7817aea74ab
Smithry9 <smithry9@oregonstate.edu>
2021-04-15 17:26:20 -0700

    added empty string and comments

--- password.py
+++ password.py
@@ -1,14 +1,15 @@
 import random
 
 def generate(x):
+    password = ""
     for i in range(x):
         charType = randInt(1,3)
         if(charType == 1):
-
+            #Add a number
         elif(charType == 2):
-
+            #Add a Uppercase Letter
         else:
-            
+            #Add a Lowercase Letter            
 
 print("Enter a length for the password: ", end="")
 num = int(input())
