2799ad5df30da2b55df963430155fad62fa43bdc
Smithry9 <smithry9@oregonstate.edu>
2021-04-15 17:32:46 -0700

    fixed call to randint

--- password.py
+++ password.py
@@ -3,18 +3,18 @@ import random
 def generate(x):
     password = ""
     for i in range(x):
-        charType = randint(1,3)
+        charType = random.randint(1,3)
         if(charType == 1):
             #Add a number
-            char = randint(48,57)
+            char = random.randint(48,57)
             char = chr(char)
         elif(charType == 2):
             #Add a Uppercase Letter
-            char = randint(65,90)
+            char = random.randint(65,90)
             char = chr(char)
         else:
             #Add a Lowercase Letter
-            char = randint(97,122)
+            char = random.randint(97,122)
             char = chr(char)
 
         password += char
